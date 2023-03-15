c4b010eddafa2b7aa513e831e3365adf2f00ad24
Mahmoud-Bakir <mahmoudbakir21@gmail.com>
2023-03-15 20:30:45 +0200

    validated  email and password

---
+++ signup.js
@@ -0,0 +1,33 @@
+const btn=document.getElementById("submit")
+btn.addEventListener("click",signup)
+const perr=document.getElementById("perr")
+const emerr=document.getElementById("emerr")
+function signup(){
+const email=document.getElementById("email").value
+console.log(email)
+const password=document.getElementById("password").value
+console.log(password)
+const data = new FormData();
+data.append('email',email)
+data.append('password',password)
+
+if(isValidEmail(email)){
+  if(isValidPassword(password)){
+    axios.post("http://localhost/PHP-Weekend-Trip-APIs/valid.php",data)
+    .then((result)=>{
+    console.log(result)
+    })
+   .catch((err) => {
+    console.error(err);
+  });
+ }else perr.innerHTML="your password should contain 8 characters minimum, one special character minimum, at least one upper case letter"
+}else emerr.innerHTML="Make sure of the validate format of an email"
+
+}
+function isValidEmail(email) {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);}
+
+  function isValidPassword(password) {
+    const emailRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    return emailRegex.test(password);}
